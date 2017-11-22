05c90171f7994222ceef3221ca5ff3d07cc5bcd6
AdrianaTR80 <31289704+AdrianaTR80@users.noreply.github.com>
2017-11-22 13:01:19 -0600

    Add files via upload

--- coseno.cc
+++ coseno.cc
@@ -1,14 +1,11 @@
-//Adriana Tapia Ramìrez
-//22 noviembre 2017
 #include <iostream>
 #include <math.h>
 using namespace std;
 int main(){
-float a=0.15;
-float coseno;
-cout << "Dame el ángulo " << endl;
+cout << "Introduce el angulo:";
+float a;
 cin >> a;
-coseno = cos(a);
-cout << "El coseno del ángulo" << a <<" es: " << coseno << endl;
+float coseno=cos(a);
+cout << "El coseno es " << coseno << endl;
 return 0;
 }
